da5a9b1d8064e6048c05701511795f7cee3c45c6
billclindon <68729434+billclindon@users.noreply.github.com>
2020-07-24 19:06:21 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Clindon Profile</title>
+</head>
+<body>
+<?php
+$name = "Bill Clindon.M"
+echo "<h1>".$name."</h1>"
+?>
+<p>Studied in Jayaraj Annapackiam CSI college of Engineering</p>
+
+</body>
+</html>
